44c7e0a6d761549d0cf74ce332e3011a4f74e622
justEugene94 <drydude77@gmail.com>
2021-08-11 22:24:52 +0300

    Added table chat_user

---
+++ database/migrations/2021_08_11_185859_create_chat_user_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_user', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('chat_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['chat_id', 'user_id']);
+
+            $table->foreign('chat_id')
+                ->references('id')->on('chats')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_user');
+    }
+}
